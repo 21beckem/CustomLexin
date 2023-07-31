d793be796e34c184a1f8039f504f5aed0b4e38ff
Michael Becker <21beckem8@gmail.com>
2023-07-31 10:24:00 +0200

    removed useless info stuff from dictionary

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 async function fetchDictionary() {
-    let response = await fetch('folkets_sv_en_public-min.json');
+    let response = await fetch('https://21beckem.github.io/CustomLexin/folkets_sv_en_public-min.json');
     let thisJson = await response.json();
     console.log(thisJson);
+    console.log(JSON.stringify(thisJson));
 }
