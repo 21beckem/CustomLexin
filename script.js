5bda5cf833075e43f8af5330e7e00d0d0f008739
Michael Becker <21beckem8@gmail.com>
2023-07-31 11:54:00 +0200

    Plan failed. Read in using script tag instead

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 async function fetchDictionary() {
+    let alreadyDownloaded = localStorage.getItem('LexinDictionary');
+    if (alreadyDownloaded) {
+        window.BWL = JSON.parse(alreadyDownloaded);
+        return window.BWL;
+    }
     let response = await fetch('https://21beckem.github.io/CustomLexin/folkets_sv_en_public-min.json');
-    let thisJson = await response.json();
-    console.log(thisJson);
-    console.log(JSON.stringify(thisJson));
+    let BIGwordList = await response.json();
+    console.log(BIGwordList);
+    localStorage.setItem('LexinDictionary', JSON.stringify(BIGwordList));
+    window.BWL = BIGwordList;
+    return window.BWL;
 }
